abfc88f1d1c4925d841d7b8b99ed298c47d1b1b6
MohamedMaher74 <mohamedmaherfelony@gmail.com>
2024-05-19 20:52:47 +0300

    add connect & disconnect event listeners

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../public/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
